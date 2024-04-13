6ed4bdddd89a812825016bb7f7eef25f48589806
ArsNova <143403919+MKCodec@users.noreply.github.com>
2024-04-13 11:56:17 -0300

    Update mwsm.js

--- mwsm.js
+++ mwsm.js
@@ -469,13 +469,13 @@ app.post('/send-message', [
 					client.sendMessage(WhatsApp, Send).then(response => {
 						Wait = WhatsApp;
 						res.status(200).json({
-							status: true,
+							Status: "Success",
 							message: 'Bot-Mwsm : Message Sent'
 						});
 
 					}).catch(err => {
 						res.status(500).json({
-							status: false,
+							Status: "Fail",
 							message: 'Bot-Mwsm : Message was not Sent'
 						});
                                         return true;
